b06b2927099cf416e761166a88a4a5fb63f04f5f
Feng Zhou <feng.zh@gmail.com>
2017-08-16 13:07:10 +0800

    By pass no proxy in iptables

--- docker/start-any-proxy.sh
+++ docker/start-any-proxy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-PROXY_DEBUG=0
+PROXY_VERBOSE=0
 
-if [ "${DEBUG}" != false ]; then
-  PROXY_DEBUG=1
+if [ "${VERBOSE}" != false ]; then
+  PROXY_VERBOSE=1
   set -x
 fi
 
@@ -14,9 +14,13 @@ fi
 
 IPTABLE_SET=1
 
+NO_PROXY_LIST="127.0.0.1/8,192.168.0.1/16,10.0.0.1/8,${NO_PROXY},`ip route list | grep src | awk '{print $1}' | sed -e :a -e 'N;s/\n/,/;ta'`"
+
 function install_iptables() {
   iptables -t nat -I OUTPUT 1 -p tcp -m mark --mark ${IPTABLE_MARK} -j ACCEPT
-  iptables -t nat -A OUTPUT -o e+ -p tcp ! -d 127.0.0.1/8 --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
+  iptables -t nat -A OUTPUT -o e+ -p tcp --match multiport --dports ${PROXY_PORTS} -j ACCEPT -d ${NO_PROXY_LIST}
+  iptables -t nat -A OUTPUT -o e+ -p tcp --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
+  iptables -t nat -A PREROUTING -i docker+ -p tcp --match multiport --dports ${PROXY_PORTS} -j ACCEPT -d ${NO_PROXY_LIST}
   iptables -t nat -A PREROUTING -i docker+ -p tcp --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
   trap 'kill -TERM $PID; uninstall_iptables' 0 2 3 15
   IPTABLE_SET=1
@@ -29,14 +33,16 @@ function uninstall_iptables() {
   trap - 0 2 3 15
   wait $PID
   iptables -t nat -D OUTPUT -p tcp -m mark --mark ${IPTABLE_MARK} -j ACCEPT
-  iptables -t nat -D OUTPUT -o e+ -p tcp ! -d 127.0.0.1/8 --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
+  iptables -t nat -D OUTPUT -o e+ -p tcp --match multiport --dports ${PROXY_PORTS} -j ACCEPT -d ${NO_PROXY_LIST}
+  iptables -t nat -D OUTPUT -o e+ -p tcp --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
+  iptables -t nat -D PREROUTING -i docker+ -p tcp --match multiport --dports ${PROXY_PORTS} -j ACCEPT -d ${NO_PROXY_LIST}
   iptables -t nat -D PREROUTING -i docker+ -p tcp --match multiport --dports ${PROXY_PORTS} -j REDIRECT --to-port ${LISTEN_PORT}
   IPTABLE_SET=0
 }
 
 install_iptables
 
-/bin/go-any-proxy -l :${LISTEN_PORT} -p ${HTTP_PROXY} -d "127.0.0.1/8,192.168.0.1/16,10.0.0.1/8,${NO_PROXY},`ip route list | grep src | awk '{print $1}' | sed -e :a -e 'N;s/\n/,/;ta'`" -v=${PROXY_DEBUG} -f=/dev/stdout -k ${IPTABLE_MARK} &
+/bin/go-any-proxy -l :${LISTEN_PORT} -p ${HTTP_PROXY} -d ${NO_PROXY_LIST} -v=${PROXY_VERBOSE} -f=/dev/stdout -k ${IPTABLE_MARK} &
 
 PID=$!
 wait $PID
